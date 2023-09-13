0dd3912ae843e99e3451b453aebf619daf2a3942
Henry <henry2craftman@gmail.com>
2023-09-13 10:54:01 +0900

    feat: Toggle 버튼으로 Display mode & Pokeball mode 설정
    - ObjectManager.cs: OnToggleBtnClk() 험수를 통한 Toggle 버튼으로 Display mode & Pokeball mode 설정

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -1,10 +1,8 @@
 ﻿//#define UNITY_EDITOR
 #define UNITY_ANDROID
-
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 
 // 목적: ARRay를 발사하여 검출된 Plane의 정보를 받아 그 위치에 Indicator를 위치시킨다.
@@ -32,16 +30,23 @@ public class ObjectManager : MonoBehaviour
     Vector3 startPos;
     Vector3 endPos;
     public Transform pokeball;
+    public Transform pokeballOriginPos;
     Vector3 originPokeballPos;
     public float throwPowerMultiplier = 0.005f;
     public float pokeballResetTime = 3;
+
+    public Toggle toggle;
+    bool toggleFlag = false;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         raycastManager = GetComponent<ARRaycastManager>();
 
         displayObject.SetActive(false);
-        originPokeballPos = pokeball.transform.position;
+
+        toggle.isOn = false;
+
 #if UNITY_EDITOR
         editorPlane.SetActive(true);
 #elif UNITY_ANDROID
@@ -77,17 +82,21 @@ public class ObjectManager : MonoBehaviour
             {
                 Debug.DrawRay(transform.position, direction * 100, Color.red, 0.5f);
                 // 5. 레이에 충돌한 오브젝트가 바닥이라면, 바닥의 특정 지점에 displayObject를 위치시킨다.
-                if(hit.collider.name.Contains("Plane"))
-                {
-                    Debug.DrawRay(transform.position, direction * 100, Color.green, 0.5f);
 
-                    displayObject.transform.position = hit.point;
-                    displayObject.transform.rotation = hit.transform.rotation;
-                    displayObject.SetActive(true);
-                }
-                else
+                if(!toggleFlag)
                 {
-                    displayObject.SetActive(false);
+                    if (hit.collider.name.Contains("Plane"))
+                    {
+                        Debug.DrawRay(transform.position, direction * 100, Color.green, 0.5f);
+
+                        displayObject.transform.position = hit.point;
+                        displayObject.transform.rotation = hit.transform.rotation;
+                        displayObject.SetActive(true);
+                    }
+                    else
+                    {
+                        displayObject.SetActive(false);
+                    }
                 }
             }
 
@@ -96,15 +105,17 @@ public class ObjectManager : MonoBehaviour
         // 목적3: 클릭(터치) 상태라면 오브젝트를 변화량 만큼 Y축으로 회전시킨다.
         else if (Input.GetMouseButton(0)) 
         {
-            deltaPos = (Input.mousePosition - prevPos);
-            
-            // y축으로 변화량 만큼 회전시킨다.
-            displayObject.transform.Rotate(transform.up, -deltaPos.normalized.x * rotationScaleMultiplier);
+            if (!toggleFlag)
+            {
+                deltaPos = (Input.mousePosition - prevPos);
 
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                // y축으로 변화량 만큼 회전시킨다.
+                displayObject.transform.Rotate(transform.up, -deltaPos.normalized.x * rotationScaleMultiplier);
+            }
+            else if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 Debug.DrawRay(transform.position, direction * 100, Color.red, 0.5f);
-                // 5. 레이에 충돌한 오브젝트가 바닥이라면, 바닥의 특정 지점에 displayObject를 위치시킨다.
+
                 if (hit.collider.name == "Pokeball" && pokeball != null)
                 {
                     pokeball.transform.position = new Vector3(hit.point.x, hit.point.y, pokeball.transform.position.z);
@@ -114,7 +125,7 @@ public class ObjectManager : MonoBehaviour
         // 목적4: 포켓볼을 드래그&드랍으로 던지고 싶다.
         else if (Input.GetMouseButtonUp(0))
         {
-            if (pokeball == null)
+            if (pokeball == null && !toggleFlag)
                 return;
 
             endPos = Input.mousePosition;
@@ -157,33 +168,96 @@ public class ObjectManager : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
 
-            if(touch.phase == TouchPhase.Began)
+            // 내 스크린 스페이스의 클릭된 지점으로 부터 레이를 발사한다.
+            // 1. 스크린을 터치한 좌표
+            Vector3 touchPos = new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane);
+            // 2. 스크린의 터치한 좌표를 World Point로
+            Vector3 touchWorldPos = Camera.main.ScreenToWorldPoint(touchPos);
+            // 3. 방향을 설정
+            Vector3 direction = (touchWorldPos - Camera.main.transform.position).normalized;
+            // 4. 해당 방향으로 레이를 쏜다.
+            Ray ray = new Ray(Camera.main.transform.position, direction);
+            RaycastHit hit;
+
+
+            if (touch.phase == TouchPhase.Began)
             {
-                // indicator가 plane을 찾으면
-                if(indicator.activeSelf)
+                if (!toggleFlag)
                 {
-                    displayObject.transform.position = indicator.transform.position;
-                    displayObject.transform.rotation = indicator.transform.rotation;
-                    displayObject.SetActive(true);
+                    // indicator가 plane을 찾으면
+                    if (indicator.activeSelf)
+                    {
+                        displayObject.transform.position = indicator.transform.position;
+                        displayObject.transform.rotation = indicator.transform.rotation;
+                        displayObject.SetActive(true);
+                    }
                 }
+
+                startPos = touch.position;
             }
             // 목적3: 클릭(터치) 상태라면 오브젝트를 변화량 만큼 Y축으로 회전시킨다.
             else if (touch.phase == TouchPhase.Moved)
             {
                 Vector3 deltaPos = touch.deltaPosition;
+                if (!toggleFlag)
+                {
+                    // y축으로 변화량 만큼 회전시킨다.
+                    displayObject.transform.Rotate(transform.up, -deltaPos.normalized.x * rotationScaleMultiplier);
+                }
+                else
+                {
+                    if(Physics.Raycast(ray, out hit, Mathf.Infinity)) 
+                    {
+                        if(hit.transform.name == "Pokeball" && pokeball != null)
+                        {
+                            pokeball.transform.position = new Vector3(hit.point.x, hit.point.y, pokeball.transform.position.z);
+                        }
+                    }
+                }
+            }
+            else if(touch.phase == TouchPhase.Ended)
+            {
+                if (pokeball == null || !toggleFlag)
+                    return;
 
-                // y축으로 변화량 만큼 회전시킨다.
-                displayObject.transform.Rotate(transform.up, -deltaPos.normalized.x * rotationScaleMultiplier);
+                endPos = touch.position;
+                Vector3 deltaPos = endPos - startPos;
+                float throwPower = deltaPos.magnitude;
+
+                // 목적4: 포켓폴을 던지고 싶다.
+                pokeball.GetComponent<Rigidbody>().useGravity = true;
+                pokeball.GetComponent<Rigidbody>().AddForce(direction * throwPower * throwPowerMultiplier, ForceMode.Impulse);
+                Invoke("ResetPokeball", pokeballResetTime);
             }
         }
     }
 
     void ResetPokeball()
     {
-        pokeball.position = originPokeballPos;
-        pokeball.rotation = Quaternion.identity;
+        pokeball.position = pokeballOriginPos.position;
+        pokeball.rotation = pokeballOriginPos.rotation;
         pokeball.GetComponent<Rigidbody>().velocity = Vector3.zero;
         pokeball.GetComponent<Rigidbody>().angularVelocity= Vector3.zero;
         pokeball.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    public void OnToggleBtnClk()
+    {
+        if(toggle.isOn)
+        {
+            pokeball.gameObject.SetActive(true);
+            displayObject.gameObject.SetActive(false);
+
+            toggleFlag = true;
+        }
+        else
+        {
+            displayObject.gameObject.SetActive(true);
+            pokeball.gameObject.SetActive(false);
+
+            toggleFlag = false;
+        }
+
+
     }
 }
